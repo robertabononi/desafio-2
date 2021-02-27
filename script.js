d708047cc436dbc70c580c14426bf89faf9688a7
robertabononi <roberta.bononi16@gmail.com>
2021-02-27 19:59:43 -0300

    adding: content on the modal

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const cards = document.querySelectorAll('.card');
 
 for (let card of cards) (
     card.addEventListener('click', function() {
-        modalOverlay.classList.add('active')
+        modalOverlay.classList.add('active');
     })
 )
 
 document.querySelector('.close-modal').addEventListener('click', function() {
-        modalOverlay.classList.remove('active');
-    })
+    modalOverlay.classList.remove('active');
+})
+
+const modalContent = document.querySelector('.modal-content');
+const maximizeModal = document.querySelector('.maximize-modal');
+
+maximizeModal.addEventListener('click', function() {
+    modalContent.classList.add('active');
+})
