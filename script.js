5c86245c23ffe48bb2a06db74df6185a929c062c
robertabononi <roberta.bononi16@gmail.com>
2021-02-28 23:58:14 -0300

    modal: opening the correct post

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const cards = document.querySelectorAll('.card');
 
 for (let card of cards) (
     card.addEventListener('click', function() {
+        const postId = card.getAttribute('id');
         modalOverlay.classList.add('active');
+        modalOverlay.querySelector('iframe').src = `https://blog.rocketseat.com.br/${postId}/`;
     })
 )
 
